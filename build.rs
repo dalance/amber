d7a81bb5572c57bdfa5af003f5b551a75e4d06f4
dalance <dalance@gmail.com>
2016-01-22 21:43:08 +0900

    fix build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("cargo:rustc-link-lib=pthread");
+    //println!("cargo:rustc-link-lib=pthread");
+    println!("cargo:rustc-cfg=-static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic");
 }
