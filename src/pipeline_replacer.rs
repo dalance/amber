c191f18f5b6573741cc8cafbddfd644fe17b7561
Steven Allen <steven@stebalien.com>
2016-02-11 15:34:00 -0500

    Use the provided NamedTempFile::persist method
    Renaming may not always work and will cause problems on Windows (the
    `persist` method marks the file as non-temporary).

--- src/pipeline_replacer.rs
+++ src/pipeline_replacer.rs
@@ -125,7 +125,7 @@ impl SimplePipelineReplacer {
             let metadata = try!( fs::metadata( &pm.path ) );
 
             try!( fs::set_permissions( tmpfile.path(), metadata.permissions() ) );
-            try!( fs::rename( tmpfile.path(), &pm.path ) );
+            try!( tmpfile.persist( &pm.path ) );
 
             Ok(())
         } );
