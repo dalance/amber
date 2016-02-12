febe174c6bebe895796a935b8bd93a7d8c4da6b4
dalance <dalance@gmail.com>
2016-02-12 22:50:23 +0900

    update compare script

--- compare.sh
+++ compare.sh
@@ -3,6 +3,8 @@
 cnt=3
 out="result.txt"
 
+prgs=(grep ack ag pt hw sift ambs)
+
 opt_grep="--binary-files=without-match --color=auto -r"
 opt_ack="--nogroup"
 opt_ag="--nogroup"
@@ -13,64 +15,55 @@ opt_ambs=""
 
 echo "" > $out
 
-grep --version >> $out
-ack  --version >> $out
-ag   --version >> $out
-pt   --version >> $out
-hw   --version >> $out
-sift --version >> $out
-ambs --version >> $out
+for p in $prgs; do
+    eval `echo "$p --version >> $out"`
+done
 
 echo "\nmany files and many matches" >> $out
-for i in {0..$cnt}; do eval `echo "( time grep $opt_grep EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`; done
-#for i in {0..$cnt}; do eval `echo "( time ack  $opt_ack  EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ag   $opt_ag   EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time pt   $opt_pt   EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time hw   $opt_hw   EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time sift $opt_sift EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ambs $opt_ambs EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`; done
+for p in $prgs; do
+    for i in {0..$cnt}; do
+        opt=`eval echo '$opt_'$p`;
+        eval `echo "( time $p $opt EXPORT_SYMBOL_GPL ./data/linux ) 2>> $out"`;
+    done;
+done
 
 echo "\nmany files and few matches" >> $out
-for i in {0..$cnt}; do eval `echo "( time grep $opt_grep irq_bypass_register_producer ./data/linux ) 2>> $out"`; done
-#for i in {0..$cnt}; do eval `echo "( time ack  $opt_ack  irq_bypass_register_producer ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ag   $opt_ag   irq_bypass_register_producer ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time pt   $opt_pt   irq_bypass_register_producer ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time hw   $opt_hw   irq_bypass_register_producer ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time sift $opt_sift irq_bypass_register_producer ./data/linux ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ambs $opt_ambs irq_bypass_register_producer ./data/linux ) 2>> $out"`; done
+for p in $prgs; do
+    for i in {0..$cnt}; do
+        opt=`eval echo '$opt_'$p`;
+        eval `echo "( time $p $opt irq_bypass_register_producer ./data/linux ) 2>> $out"`;
+    done;
+done
 
 echo "\nmany files and many matches with binary" >> $out
-for i in {0..$cnt}; do eval `echo "( time grep $opt_grep EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`; done
-#for i in {0..$cnt}; do eval `echo "( time ack  $opt_ack  EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ag   $opt_ag   EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time pt   $opt_pt   EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time hw   $opt_hw   EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time sift $opt_sift EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ambs $opt_ambs EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`; done
+for p in $prgs; do
+    for i in {0..$cnt}; do
+        opt=`eval echo '$opt_'$p`;
+        eval `echo "( time $p $opt EXPORT_SYMBOL_GPL ./data/linux_build ) 2>> $out"`;
+    done;
+done
 
 echo "\nmany files and few matches with binary" >> $out
-for i in {0..$cnt}; do eval `echo "( time grep $opt_grep irq_bypass_register_producer ./data/linux_build ) 2>> $out"`; done
-#for i in {0..$cnt}; do eval `echo "( time ack  $opt_ack  irq_bypass_register_producer ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ag   $opt_ag   irq_bypass_register_producer ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time pt   $opt_pt   irq_bypass_register_producer ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time hw   $opt_hw   irq_bypass_register_producer ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time sift $opt_sift irq_bypass_register_producer ./data/linux_build ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ambs $opt_ambs irq_bypass_register_producer ./data/linux_build ) 2>> $out"`; done
+for p in $prgs; do
+    for i in {0..$cnt}; do
+        opt=`eval echo '$opt_'$p`;
+        eval `echo "( time $p $opt irq_bypass_register_producer ./data/linux_build ) 2>> $out"`;
+    done;
+done
 
 echo "\nlarge file and many matches" >> $out
-for i in {0..$cnt}; do eval `echo "( time grep $opt_grep 検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-#for i in {0..$cnt}; do eval `echo "( time ack  $opt_ack  検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ag   $opt_ag   検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time pt   $opt_pt   検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time hw   $opt_hw   検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time sift $opt_sift 検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ambs $opt_ambs 検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
+for p in $prgs; do
+    for i in {0..$cnt}; do
+        opt=`eval echo '$opt_'$p`;
+        eval `echo "( time $p $opt 検索結果 ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`;
+    done;
+done
 
 echo "\nlarge file and few matches" >> $out
-for i in {0..$cnt}; do eval `echo "( time grep $opt_grep \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-#for i in {0..$cnt}; do eval `echo "( time ack  $opt_ack  \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ag   $opt_ag   \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time pt   $opt_pt   \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time hw   $opt_hw   \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time sift $opt_sift \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
-for i in {0..$cnt}; do eval `echo "( time ambs $opt_ambs \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`; done
+for p in $prgs; do
+    for i in {0..$cnt}; do
+        opt=`eval echo '$opt_'$p`;
+        eval `echo "( time $p $opt \"Quick Search\" ./data/jawiki-latest-pages-articles.xml ) 2>> $out"`;
+    done;
+done
+
